1d3741df0ba4b2318512339ab2dc7791a577f970
LoudSilencex666 <icelookpl@gmail.com>
2017-08-27 16:39:23 +0200

    ver 0.0.0.

--- game.js
+++ game.js
@@ -3,22 +3,26 @@
         const renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.view);
        
-        let texture = [];
-        texture[0] = ["files/images/test/test.jpeg"];
+        
         
 
         PIXI.loader
-        .add(texture[0])
+        .add("testTileset", "files/images/testTilesets/testName.json")
         .load(setup);
 
-
+        
       
         function setup() {
-            let sprite = new PIXI.Sprite(
-            PIXI.loader.resources[texture[0]].texture
-            );
-
-            stage.addChild(sprite);;
+            let testTexture = PIXI.loader.resources["testTileset"].textures;
+            let sprite1 = new PIXI.Sprite(testTexture["test1.png"]);
+            let sprite2 = new PIXI.Sprite(testTexture["test2.png"]);
+            let sprite3 = new PIXI.Sprite(testTexture["test3.png"]);
+            
+            sprite1.position.set(100, 100);
+            sprite2.position.set(600, 400);
+            sprite3.position.set(1000, 700);
+            
+            stage.addChild(sprite1, sprite2, sprite3);
             
             renderer.render(stage);
         }
