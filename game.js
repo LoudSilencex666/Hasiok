8a00a4ab1a5c5976ae471c1b3f742c8801107fde
LoudSilencex666 <icelookpl@gmail.com>
2017-08-28 01:42:01 +0200

    ver. 0.0.0.

--- game.js
+++ game.js
@@ -1,24 +1,24 @@
-        let mainContainer = new PIXI.Container(); // Tworzenie głównego containera który na końcu będziemy wyświetlać.
-        const renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight); //rozmiar canvasa szerokość i wysokość
-        document.body.appendChild(renderer.view); //dodawnie canvasu do documentu HTML
+        let mainContainer = new PIXI.Container(); 
+        const renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.view); 
 
         PIXI.loader
         .add("menuTileset", "files/images/menuTilesets/menuTileset.json")
         .load(setup);
 
-        let idMenuTexture; // randomowe zmienne które bedziemy używać
+        let idMenuTexture; 
 
-        let menu = { //obiekt menu
-            x : 0, //koordynat x
-            y : 0, //koordynat y
-            background : {}, //obiekt tła
-            backgroundInitialization : function() { //generowanie tła na ekranie gry
-                idMenuTexture = PIXI.loader.resources["menuTileset"].textures; // odnośnik by nie musieć pisać tego PIXI (patrz forma 3 w poradniku o displejowaniu spritów z tileseta)
-                this.background = new PIXI.Sprite(idMenuTexture["menuBackground.png"]); //Tworzenie Sprita i przypisanie go do nazwy background
-                this.background.position.set(this.x, this.y); //pozycja backgroundu
-                this.background.width = window.innerWidth; //szerokość
-                this.background.height = window.innerHeight; //wysokość
-                mainContainer.addChild(this.background); //dodawanie sprite'a (backgroundu) do głównego containera -to co w głównym się znajdzie to zostaje pokazanie na ekranie
+        let menu = {
+            x : 0, 
+            y : 0,
+            background : {}, 
+            backgroundInitialization : function() { 
+                idMenuTexture = PIXI.loader.resources["menuTileset"].textures; 
+                this.background = new PIXI.Sprite(idMenuTexture["menuBackground.png"]); 
+                this.background.position.set(this.x, this.y); 
+                this.background.width = window.innerWidth; 
+                this.background.height = window.innerHeight; 
+                mainContainer.addChild(this.background); 
             },
 
             musicX : 898,
@@ -56,9 +56,9 @@
         }
       
         function setup() {
-            menu.backgroundInitialization(); //wywołanie funkcji backgroundu, jeśli nie wywołamy w setupie to instrukcja nigdy sie nie wykona 
+            menu.backgroundInitialization(); 
             menu.musicInitialization();
             menu.newGameInitialization();    
             menu.profilInitialization();
-            renderer.render(mainContainer); //ukazanie na ekranie naszego mainContainer
+            renderer.render(mainContainer); 
         }
