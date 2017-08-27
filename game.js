721d256688bdae466905a8304d0b1fd9281f8acd
LoudSilencex666 <icelookpl@gmail.com>
2017-08-28 00:04:22 +0200

    ver. 0.0.0.

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 
-        let stage = new PIXI.Container();
+        let mainContainer = new PIXI.Container();
         const renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.view);
 
@@ -7,30 +7,26 @@
         .add("menuTileset", "files/images/menuTilesets/menuTileset.json")
         .load(setup);
 
-        let idMenuTexture, background;
+        let idMenuTexture;
 
         let menu = {
             x : 0,
             y : 0,
             background : {},
-            initialization: function() {
+            initialization : function() {
                 idMenuTexture = PIXI.loader.resources["menuTileset"].textures;
                 this.background = new PIXI.Sprite(idMenuTexture["menuBackground.png"]);
                 this.background.position.set(this.x, this.y);
                 this.background.width = window.innerWidth;
                 this.background.height = window.innerHeight;
-                stage.addChild(this.background);
+                mainContainer.addChild(this.background);
             }
         }
-
-       
-
-
       
         function setup() {
             menu.initialization();        
             
-            renderer.render(stage);
+            renderer.render(mainContainer);
         }
         
         
