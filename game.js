ee22ea71d73a5276f32c0fc83527c736e5988e93
Jaskarnet <Jaskarnet247@gmail.com>
2017-08-28 02:25:15 +0200

    ver. 0.0.0.

--- game.js
+++ game.js
@@ -62,8 +62,31 @@ let menu = {
         this.profil.height = this.profilH;
         this.profil.position.set(window.innerWidth/2 - this.profil.width/2, window.innerHeight/10 * 4  - this.profil.height/2); 
         mainContainer.addChild(this.profil);
-    }
+    },
 
+    tutorialW : 200,
+    tutorialH : 80,
+    tutorial : {},
+    tutorialInitialization : function() {
+        idMenuTexture = PIXI.loader.resources["menuTileset"].textures;
+        this.tutorial = new PIXI.Sprite(idMenuTexture["welcome_twitch.png"]);
+        this.tutorial.width = this.tutorialW;
+        this.tutorial.height = this.tutorialH;
+        this.tutorial.position.set(window.InnerWidth/2 - this.tutorial.width/2, window.innerheight/10 * 6 - this.tutorial.height/2);
+        mainContainer.addChild(this.tutorial);
+    },
+
+    creditsW : 200,
+    creditsH : 80,
+    credits : {},
+    creditsInitialization : function() {
+        idMenuTexture = PIXI.loader.resources["menuTileset"].textures;
+        this.credits = new PIXI.Sprite(idMenuTexture["welcome_twitch.png"]);
+        this.credits.width = this.creditsW;
+        this.credits.height = this.creditsH;
+        this.credits.position.set(window.InnerWidth/2 - this.credits.width/2, window.innerheight/10 * 9 - this.credits.height/2);
+        mainContainer.addChild(this.credits);
+    }
 }
 
 let initializations = function() {
@@ -71,6 +94,8 @@ let initializations = function() {
     menu.musicInitialization();
     menu.newGameInitialization();    
     menu.profilInitialization();
+    menu.tutorialInitialization();
+    menu.creditsInitialization();
 }
 
 function setup() {
