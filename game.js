9da5b9c4c52547fd079493baa9b7632878c602c6
Jaskarnet <Jaskarnet247@gmail.com>
2017-09-03 05:50:16 +0200

    ver. 0.0.0.

--- game.js
+++ game.js
@@ -10,8 +10,8 @@ PIXI.loader
 .load(setup);
 
 /////////////// * GlobalneZmienne *  //////////////////////////
-
-
+let menuStatement = true;
+let gameStatement = false;
 
 /////////////// * Obiekt całego Menu *  //////////////////////////
 
@@ -42,7 +42,9 @@ let menu = {
         this.logo.position.set(window.innerWidth/2 - this.logo.width/2, 0); 
         mainContainer.addChild(this.logo);
     },
+
 /////////////////////// drugie okna w menu //////////////////////////
+
     secondWindow : {
         backgroundW : window.innerWidth,
         backgroundH : window.innerHeight,
@@ -77,8 +79,24 @@ let menu = {
 
             this.backButton.click = function() {
                 menuInitializations();
+                
                 menu.secondWindow.background.destroy();
                 menu.secondWindow.backButton.destroy();
+                
+                if(menu.newGameWindowStatement) {
+                    menu.newGameWindow.playersListSection.destroy();
+                    menu.newGameWindow.mapsSection.destroy();
+                    menu.newGameWindow.mapsReviewSection.destroy();
+
+                    for(i = 0; i < 7; i++) {
+                        menu.menuPlayers[i].playerSection.destroy();
+                        menu.menuPlayers[i].playerSectionContent.destroy();
+                    }
+
+                    for(i = 0; i < 10; i++) {
+                        menu.mapsPositioners[i].mapPositioner.destroy();
+                    }
+                }
 
                 menu.newGameWindowStatement = false;
                 menu.profilWindowStatement = false;
@@ -95,6 +113,7 @@ let menu = {
     newGameH : 100,
     newGame : {},
     newGameInitialization : function() {
+
         this.newGame = new PIXI.Sprite.fromFrame("buttonNewGame.png"); 
         this.newGame.interactive = true;
         this.newGame.buttonMode = true;     
@@ -121,6 +140,15 @@ let menu = {
 
     newGameWindowStatement : false,
     menuPlayers : [],
+    menuPlayersData : [
+        "Janek",
+        "Janusz",
+        "Brzoza",
+        "Brzozka",
+        "Damian",
+        "Dariusz134",
+        "Wilgotny Zbigniew"
+    ],
     mapsPositioners : [],
     newGameWindow : {
         playersListSection : {},
@@ -144,8 +172,9 @@ let menu = {
                 this.playerSection.width = 480;
                 this.playerSection.height = 75;
                 this.playerSection.position.set(menu.newGameWindow.playersListSection.x + 10, menu.newGameWindow.playersListSection.y + 30);
-                mainContainer.addChild(this.playerSection);
-                
+                this.playerSectionContent = new PIXI.Text("",{fontFamily : 'Arial', fontSize: 20, fill : "white", align : 'center'});
+                this.playerSectionContent.position.set(this.playerSection.x + 50, this.playerSection.y + this.playerSection.height/2 - this.playerSectionContent.height/2);
+                mainContainer.addChild(this.playerSection, this.playerSectionContent);
             }
 
             playerSectionFunctionality() {
@@ -223,11 +252,54 @@ let menu = {
             this.startSection = new PIXI.Sprite.fromImage("files/images/menuPics/placeHolder.png");
             this.startSection.width = 350;
             this.startSection.height = 100;
+            this.startSection.interactive = true;
+            this.startSection.buttonMode = true;
             this.startSection.position.set(window.innerWidth/2 - this.startSection.width/2, window.innerHeight/1.2);
             mainContainer.addChild(this.startSection);
         },
         startSectionFunctionality : function() {
+            this.startSection.mouseover = function() {
+                this.texture = PIXI.Texture.fromFrame("buttonBackHover.png");
+            };
+        
+            this.startSection.mouseout = function() {
+                this.texture = PIXI.Texture.fromFrame("buttonBack.png");
+            };
 
+            this.startSection.click = function() {
+                
+                menuStatement = false;
+                // usuwanie newGameWindow 
+                menu.secondWindow.background.destroy();
+                menu.secondWindow.backButton.destroy();
+                
+                if(menu.newGameWindowStatement) {
+                    menu.newGameWindow.playersListSection.destroy();
+                    menu.newGameWindow.mapsSection.destroy();
+                    menu.newGameWindow.mapsReviewSection.destroy();
+                    menu.newGameWindow.startSection.destroy();
+
+                    for(i = 0; i < 7; i++) {
+                        menu.menuPlayers[i].playerSection.destroy();
+                        menu.menuPlayers[i].playerSectionContent.destroy();
+                    }
+
+                    for(i = 0; i < 10; i++) {
+                        menu.mapsPositioners[i].mapPositioner.destroy();
+                    }
+                }
+
+                menu.newGameWindowStatement = false;
+                menu.profilWindowStatement = false;
+                menu.tutorialWindowStatement = false;
+                menu.musicWindowStatement = false;
+                menu.creditsWindowStatement = false;
+
+                // inicjalizacja gry
+                gameInitializations();
+                gameStatement = true;
+
+            }
         },
     },
 
@@ -478,6 +550,35 @@ let menu = {
 
 };
 
+/////////////// * Obiekt Planety *  //////////////////////////
+
+let planets = {
+
+/////////////////////// testowa planeta  //////////////////////////
+
+    planetsTest : [],
+    PlanetTest : class {
+        constructor() {
+            this.planetTest = new PIXI.Sprite.fromImage("files/images/menuPics/placeHolder.png");
+            this.planetTest.interactive = true;
+            this.planetTest.width = 80;
+            this.planetTest.height = 80;
+            this.planetTest.position.set(0, 0);
+            mainContainer.addChild(this.planetTest);
+        }
+   
+        planetTestFunctionality() {
+            this.planetTest.mouseover = function() {
+                this.alpha -= 1;
+            };
+
+            this.planetTest.mouseout = function() {
+                this.alpha += 1;
+            };
+        }
+    },
+}
+
 /////////////// * Grupowane Inicjalizacje elementów Menu *  //////////////////////////
 /////////////////////// Całego Menu //////////////////////////
 let menuInitializations = function() {
@@ -496,7 +597,10 @@ let menuNewGameWindowInitializations = function() {
 
     for(i = 0; i < 7; i++) {
         menu.menuPlayers[i] = new menu.newGameWindow.PlayerSection();
+        menu.menuPlayersData[i] = menu.menuPlayersData[i].toUpperCase();
+        menu.menuPlayers[i].playerSectionContent.text = menu.menuPlayersData[i];
         menu.menuPlayers[i].playerSection.y += 70 * i;
+        menu.menuPlayers[i].playerSectionContent.y += 70 * i;
     }
             
     menu.newGameWindow.mapsSectionInitialization();
@@ -506,18 +610,33 @@ let menuNewGameWindowInitializations = function() {
         menu.mapsPositioners[i].mapPositioner.y += 50 * i;
     }
 
-    console.log(menu.mapsPositioners);
+    console.log(menu.menuPlayersData[6]);
     menu.newGameWindow.mapsReviewSectionInitialization();
     menu.newGameWindow.startSectionInitialization();
     menu.secondWindow.backButtonInitialization();
 };
 
 let menuNewGameWindowDestroyers = function() {
+    menu.background.destroy();
+    menu.logo.destroy();
     menu.newGame.destroy();
     menu.profil.destroy();
     menu.tutorial.destroy();
     menu.music.destroy();
     menu.credits.destroy();
+}
+
+/////////////// * Grupowane Inicjalizacje elementów Game *  //////////////////////////
+/////////////////////// Całego Game //////////////////////////
+let gameInitializations = function() {
+    for(i = 0; i < 15; i++) {
+        planets.planetsTest[i] = new planets.PlanetTest();
+        planets.planetsTest[i].planetTest.x = 50 * i;
+        planets.planetsTest[i].planetTest.y = 50 * i;
+    }
+
+    console.log(planets.planetsTest);
+    
 }
 
 /////////////// * Grupowane Funkcjonalności elementów Menu do gameInitiate *  //////////////////////////
@@ -535,6 +654,8 @@ let menuFunctionalities = function() {
         for(i = 0; i < 10; i++) {    
             menu.mapsPositioners[i].mapPositionerFunctionality();
         }
+
+        menu.newGameWindow.startSectionFunctionality();
 
     }
 
@@ -561,6 +682,14 @@ let menuFunctionalities = function() {
     
 };
 
+/////////////// * Grupowane Funkcjonalności elementów Game do gameInitiate *  //////////////////////////
+
+let gameFunctionalities = function() {
+    for(i = 0; i < 15; i++) {
+        planets.planetsTest[i].planetTestFunctionality();
+    }
+}
+
 /////////////// * Funkcje Uruchamiające grę *  //////////////////////////
 
 function setup() {
@@ -570,9 +699,43 @@ function setup() {
 };
 
 function gameInitiate(){
-      requestAnimationFrame(gameInitiate);
+    requestAnimationFrame(gameInitiate);
+    if(menuStatement) {
+        menuFunctionalities();
+    }
 
-      menuFunctionalities();
+    if(gameStatement) {
+        gameFunctionalities();
+    }
 
-      renderer.render(mainContainer);
+    renderer.render(mainContainer);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Janek jest super eksdi
